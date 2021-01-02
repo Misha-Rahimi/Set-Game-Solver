3db29218c03e0d50d091f66f7613d716ae4e9514
Misha-Rahimi <mlrahimi@purdue.edu>
2021-01-02 17:52:52 -0500

    Fixing things

---
+++ src/Card.java
@@ -0,0 +1,71 @@
+/**
+ * Class representing a card.
+ */
+
+public class Card {
+    private CardColor color;
+    private CardShading shading;
+    private CardShape shape;
+    private int number;
+    private CardBounds cardBounds;
+    private int currentNumSets;
+
+    public Card() {
+        color = CardColor.NONE;
+        shading = CardShading.NONE;
+        shape = CardShape.NONE;
+        number = 0;
+        currentNumSets = 1;
+    }
+
+    public CardColor getColor() {
+        return color;
+    }
+
+    public CardShading getShading() {
+        return shading;
+    }
+
+    public CardShape getShape() {
+        return shape;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public CardBounds getCardBounds() {
+        return cardBounds;
+    }
+
+    public int getCurrentNumSets() {
+        return currentNumSets;
+    }
+
+    public void setColor(CardColor newColor) {
+        color = newColor;
+    }
+
+    public void setShading(CardShading newShading) {
+        shading = newShading;
+    }
+
+    public void setShape(CardShape newShape) {
+        shape = newShape;
+    }
+
+    public void setNumber(int newNumber) {
+        number = newNumber;
+    }
+
+    public void setCardBounds(CardBounds cardBounds) {
+        this.cardBounds = cardBounds;
+    }
+
+    public void incrementNumSets() {
+        currentNumSets++;
+    }
+
+
+
+}
